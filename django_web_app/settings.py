8c7e07ea962bfbeb0e4a609a5b8e3bbf8ff2f219
Snouden0071 <144625437+bekzodsindorov1524@users.noreply.github.com>
2023-11-05 14:25:40 +0500

    s
    a

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -119,10 +119,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-# STATIC_ROOT= os.path.join(BASE_DIR, 'static'),
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -131,10 +127,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog/static'),
     # boshqa static papkalar...
 ]
+
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage' # new
 
-# MEDIA FILES
+# MEDIA fayllar
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
